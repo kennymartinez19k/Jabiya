cb17fa8dcb6b0522f0451cf57c85f9ebbbd2ced0
kennymartinez19k <kennymartinez19k@gmail.com>
2021-11-12 13:39:50 -0400

    login and registration

---
+++ src/components/Buttons/BackButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <span @click="goBack()" uk-icon="icon: reply"></span><strong></strong>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+<style scoped>
+span{
+  position: absolute;
+  top: 21px;
+  left: 23px;
+  transform: scale(1.2);
+  z-index: 1;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login uk-flex uk-flex-column .uk-height-1-1">
+    <img src="../assets/logo.png" class="img" style="width: 100px" alt="">
+    <div class="uk-margin-auto-top">
+        <button @click="changeRoute('sign-in')" class="uk-button uk-button-orange uk-width-1-1*@s uk-margin-small-bottom" style="margin-top: 15px">Entrar a tu cuenta</button>
+        <button @click="changeRoute('sign-up')" class="uk-button uk-button-purple uk-width-1-1 uk-margin-small-bottom" style="margin-top: 15px">Registrarse</button>
+    </div>
+   </div>
+</template>
+
+<script>
+
+export default {
+    mounted(){
+        this.$store.commit('setCurrent', false)
+    },
+    methods:{
+        changeRoute (path) {
+            this.$router.push({ name: path }).catch(() => {})
+        }
+    }
+  
+
+
+}
+</script>
+
+<style scoped>
+.login{
+    padding: 15px;
+}
+img{
+  display: block;
+  margin: 10px auto;
+  width: 50%;
+}
+
+
+</style>
