109adc00999876c8a25064d9a16401201d758ebc
kennymartinez19k <kennymartinez19k@gmail.com>
2021-11-12 13:55:13 -0400

    login

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import storeModule  from './store/index'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import {IonicVue} from '@ionic/vue';
 import '@ionic/core/css/ionic.bundle.css';
-
+library.add(fas)
 const store = createStore( {...storeModule})
 
 createApp(App)
