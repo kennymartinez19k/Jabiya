89e28b25501b8def6fc6bd32220bc058843ed0fb
wilsonSoto <77066800+wilsonSoto@users.noreply.github.com>
2021-11-15 18:13:32 -0400

    in the process of submitting orders

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,91 +1,23 @@
 <template>
-  <div class="uk-flex uk-flex-column uk-flex-between cnt">
-      <div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
-            </div>
+  <div class=" cnt uk-flex uk-flex-column uk-flex-between">
+      <div class="uk-card uk-card-default uk-width-1-2@m container">
+        <div
+          v-for="order in orders"
+          :key="order"
+          class="uk-card uk-card-default uk-card-body uk-flex uk-flex-between uk-margin-top"
+        >
+          <div class="uk-text-left info-user">
+           <p class="uk-margin-remove"><strong>Numero de Orden:</strong>&nbsp;<span class="uk-text-light">{{order.numberOrder}}</span></p>
+            <p class="uk-margin-remove"><strong>Numero de cajas:</strong>&nbsp;<span class="uk-text-light"></span>{{order.boxNumber}}</p>
+            <p class="uk-margin-remove"><strong>Nombre del cliente:</strong>&nbsp;<span class="uk-text-light">{{order.customerName}}</span></p>
+            <p class="uk-margin-remove"><strong>Dirección</strong>&nbsp;<span class="uk-text-light">{{order.direction}}</span></p>
+            <p class="uk-margin-remove"><strong>Tiempo esperado:</strong>&nbsp;<span class="uk-text-light">{{order.waitTime}}</span></p>
           </div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
+            <div>
+                <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
             </div>
-          </div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
-            </div>
-          </div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
-            </div>
-          </div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
-            </div>
-          </div>
-          <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-bold uk-text-left">
-                    <p class="uk-margin-remove">Numero de Orden: <span class="uk-text-light">1123213123</span></p>
-                    <p class="uk-margin-remove">Numero de cajas: <span class="uk-text-light">1</span></p>
-                    <p class="uk-margin-remove">Nombre del cliente: <span class="uk-text-light">Juan Perez</span></p>
-                    <p class="uk-margin-remove">Direccion <span class="uk-text-light">Republica de colombia</span></p>
-                    <p class="uk-margin-remove">Tiempo esperado: <span class="uk-text-light">2021-11-03</span></p>
-                    <div>
-                        <button class="uk-button uk-text-light uk-button-purple">Escanear</button>
-                    </div>
-                </div>
-            </div>
-          </div>
-          
+         
+        </div>
       </div>
     
         <slide-unlock
@@ -94,14 +26,14 @@
         :circle="true"
         :disabled="false"
         :noanimate="false"
-        :width="400"
-        :height="40"
+        :height="50"
         :completedBg="completed"
         class="slide"
-        text="Escaneo Corrido"
+        text="slide to unlock"
         success-text="success"
         @completed="complete()"
         textSize="10px"
+        
     />
   </div>
 </template>
@@ -111,7 +43,52 @@ import SlideUnlock from "vue-slide-unlock"
 export default {
     data(){
         return{
-            completed: 'background-color: #2a307c !important'
+            completed: 'background-color: #2a307c !important',
+            orders: [
+                {
+                    id: 1,
+                    numberOrder: '1123213123',
+                    boxNumber: 1,
+                    customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+                {
+                    id: 2,
+                    numberOrder: '1123213123',
+                    boxNumber: 1, customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+                {
+                    id: 3,
+                    numberOrder: '1123213123',
+                    boxNumber: 1, customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+                {
+                    id: 4,
+                    numberOrder: '1123213123',
+                    boxNumber: 1, customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+                {
+                     id: 5,
+                    numberOrder: '1123213123',
+                    boxNumber: 1, customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+                {
+                     id: 6,
+                    numberOrder: '1123213123',
+                    boxNumber: 1, customerName: 'Juan Perez', 
+                    direction: 'Repebublica de colombia',
+                    waitTime:  '2021-11-03'
+                },
+            ]
         }
     },
     components: {
@@ -125,15 +102,38 @@ export default {
 </script>
 
 <style scoped>
-
+.slide{
+    background: #2a307c;
+     position: fixed;
+    width: 96% !important;
+    bottom: 5px;
+    height: 40px;
+}
 .slide .slideunlock.is-complete .slideunlock-progressbar {
     background-color: #2a307c;
 }
 .slide:first-child{
     background-color: #2a307c;
 }
+.container {
+  padding: 10px 14px 5px;
+  box-shadow: 0px 0px;
+}
+.uk-card{
+  padding: 20px 10px;
+}
 .cnt {
   height: 100%;
   overflow: scroll;
 }
+.uk-button {
+  padding: 0px 5px !important;
+  font-size: 12px;
+}
+.p {
+  font-size: 13px;
+}
+
+
+
 </style>
