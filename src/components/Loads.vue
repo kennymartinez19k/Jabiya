1cbcefb42fda277cf22a3d5762b8fbae4cee1a5c
kennymartinez19k <kennymartinez19k@gmail.com>
2022-02-07 08:44:51 -0400

    loads state

--- src/components/Loads.vue
+++ src/components/Loads.vue
@@ -218,7 +218,6 @@ export default {
     async setLoad(val) {
       this.setProfile(val)
 
-      console.log('s')
       for(var i = 0; i < val.Orders.length; i++){
         let order =  await this.$services.loadsScanServices.getProduct(val.Orders[i]._id);
         val.firstOrdenInfo = val.Orders[i]
