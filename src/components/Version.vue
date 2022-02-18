4ff55b6e93e966eefb44852484838e34fef9d433
wilsonSoto <77066800+wilsonSoto@users.noreply.github.com>
2022-02-18 18:22:30 -0400

    add change id app and view version

---
+++ src/components/Version.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="uk-container">
+
+  <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-text-left uk-margin-top">
+    <h3 class="uk-card-title">Flai App <span>&reg;</span></h3>
+    <p>Numero de version: <span>{{version}} </span></p>
+    <p>Fecha: <span>{{date}} </span></p>
+</div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'Version',
+    data() {
+        return {
+            date: '18/02/2022',
+            version: '1.0.0',
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
