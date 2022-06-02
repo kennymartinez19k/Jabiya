334d4383ed0ff35695fe1c1881fb4c3545132cf9
Emy Soto <emysaulconsultingdr@gmail.com>
2022-06-02 19:00:19 -0400

    added printer

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -93,6 +93,15 @@
       >
         Iniciar sesión
       </button>
+
+       <button
+        type="button"
+        class="uk-button uk-button-purple uk-width-1-1 uk-margin-small-bottom input-item-btn"
+        @click="testing()"
+        style="margin-top: 15px"
+      >
+        Printing
+      </button>
     </form>
   </div>
 
@@ -106,7 +115,7 @@ import { LocalStorage } from "../mixins/LocalStorage";
 import {Mixins} from "../mixins/mixins"
 import { role, userType, urlEnum } from '../types'
 import { Browser} from '@capacitor/browser'
-
+import {  PrintV } from 'printv'
 
 
 export default {
@@ -192,6 +201,9 @@ export default {
     ])
   },
   methods: {
+    testing () {
+      PrintV.blPrint("hola amigos")
+    },
     async changeRoute(path) {
       if(path == 'settings-out'){
         this.$router.push({ name: path }).catch(() => {}) 
